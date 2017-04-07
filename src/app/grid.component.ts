ad66a63aa3d644612322acf7a85a92219f0bb8af
Ankur Sethi <contact@ankursethi.in>
2017-04-07 15:45:00 +0530

    Listen on scroll.

--- src/app/grid.component.ts
+++ src/app/grid.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 const itemHeightPx = 30;
+const throttleTimeMs = 200;
 
 @Component({
   selector: 'grid',
@@ -37,5 +39,29 @@ export class GridComponent  {
 
 		// Tell Angular that we changed some bindings.
 		this.cdr.detectChanges();
+
+		// Observe the scroll event on the grid container.
+		const scrollEventStream = Observable.fromEvent(this.gridContainer.nativeElement, 'scroll')
+			.throttleTime(throttleTimeMs);
+		
+		// Subscribe to the scroll event, recalculate visible elements and bookend heights.
+		scrollEventStream.subscribe(_ => {
+			const scrollTopPx = this.gridContainer.nativeElement.scrollTop;
+			
+			// Calculate the index of the first visible element, depending on how far we've scrolled.
+			// Then, set visible items.
+			const firstVisibleElementIndex = Math.floor(scrollTopPx / itemHeightPx);
+			this.visibleItems = this.items.slice(firstVisibleElementIndex, firstVisibleElementIndex + numVisibleChildren);
+			
+			// Top bookend height is same as scroll top.
+			this.topBookend.nativeElement.style.height = `${scrollTopPx}px`;
+			
+			// Bottom bookend height is total height - height of items - scroll top.
+			const bottomBookendHeightPx = (this.items.length - numVisibleChildren) * itemHeightPx - scrollTopPx;
+			this.bottomBookend.nativeElement.style.height = `${bottomBookendHeightPx}px`;
+
+			// Detect changes.
+			this.cdr.detectChanges();
+		});
 	}
 }
