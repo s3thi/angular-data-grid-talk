fc8e5f0a9625d126c1e726afcd7d91fa4a740f60
Ankur Sethi <contact@ankursethi.in>
2017-04-07 14:14:50 +0530

    Calculate container height on load and figure out number of children.

--- src/app/grid.component.ts
+++ src/app/grid.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+
+const itemHeightPx = 30;
 
 @Component({
   selector: 'grid',
   template: `
-  	<ul class="grid">
+  	<ul #gridContainer class="grid">
   		<li *ngFor="let item of items">{{ item.label }}</li>
   	</ul>
   `,
 })
 export class GridComponent  {
 	@Input() items: any;
+	@ViewChild("gridContainer") container: ElementRef;
+
+	ngAfterViewInit() {
+		const containerHeightPx = this.container.nativeElement.clientHeight;
+		const numVisibleChildren = Math.ceil(containerHeightPx / itemHeightPx);
+		console.log(numVisibleChildren);
+	}
 }
